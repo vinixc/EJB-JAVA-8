ae8ff4810e6ed42abb9f0fcd4b529f5936b21d25
Vinicius Carvalho <vinicius.tecbcc@gmail.com>
2020-03-20 20:42:11 -0300

    chamando o EJB

--- src/main/java/br/com/vini/JAXRSConfiguration.java
+++ src/main/java/br/com/vini/JAXRSConfiguration.java
@@ -6,7 +6,7 @@ import javax.ws.rs.core.Application;
 
 
 /**
- * @author rhuanrocha
+ * @author Vinicius de Carvalho
  */
 
 
---
+++ src/main/java/br/com/vini/resources/AgendamentoEmailResource.java
@@ -0,0 +1,27 @@
+package br.com.vini.resources;
+
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import br.com.vini.business.AgendamentoEmailBusiness;
+
+@Path("/agendamentoemail")
+public class AgendamentoEmailResource {
+	
+	@Inject
+	private AgendamentoEmailBusiness agendamentoEmailBusiness;
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response listarAgendamentosEmail() {
+		
+		List<String> emails = agendamentoEmailBusiness.listarAgendamentosEmail();
+		return Response.ok(emails).build();
+	}
+}

df238a72cafff609b5123679160f30b6aeeb7c7f
Vinicius Carvalho <vinicius.tecbcc@gmail.com>
2020-03-21 10:45:32 -0300

    testando o logger4j

--- src/main/java/br/com/vini/resources/AgendamentoEmailResource.java
+++ src/main/java/br/com/vini/resources/AgendamentoEmailResource.java
@@ -1,8 +1,11 @@
 package br.com.vini.resources;
 
 import java.util.List;
+import java.util.logging.Logger;
 
+import javax.ejb.EJBException;
 import javax.inject.Inject;
+import javax.validation.ConstraintViolationException;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -10,12 +13,15 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import br.com.vini.business.AgendamentoEmailBusiness;
 import br.com.vini.entity.AgendamentoEmail;
 
 @Path("/agendamentoemail")
 public class AgendamentoEmailResource {
+	
+	private static Logger logger = Logger.getLogger(AgendamentoEmailResource.class.getName());
 	
 	@Inject
 	private AgendamentoEmailBusiness agendamentoEmailBusiness;
@@ -33,9 +39,22 @@ public class AgendamentoEmailResource {
 	@Consumes(MediaType.APPLICATION_JSON)
 	public Response salvarAgendamentoEmail(AgendamentoEmail agendamentoEmail ) {
 		
-		agendamentoEmailBusiness.salvarAgendamentoEmail(agendamentoEmail);
+		try {
+			agendamentoEmailBusiness.salvarAgendamentoEmail(agendamentoEmail);
+			
+		}catch(EJBException e) {
+			if(e.getCause() instanceof ConstraintViolationException) {
+				logger.info(e.getMessage());
+			}
+			else {
+				logger.severe(e.getMessage());
+			}
+			
+			throw e;
+		}
+		
 		return Response
-				.status(201)
+				.status(Status.CREATED.getStatusCode())
 				.build();
 		
 	}
